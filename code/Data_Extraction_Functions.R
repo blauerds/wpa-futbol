79c36754f3e967c8ba18686204b4589fd827b1ce
lucasvarelamauro@gmail.com <lucasvarelamauro@gmail.com>
2023-04-07 01:34:53 -0300

    Changed the match reports functions, time reduction.

--- code/Data_Extraction_Functions.R
+++ code/Data_Extraction_Functions.R
@@ -1,7 +1,6 @@
 library(worldfootballR)
 library(tidyverse)
 library(dplyr)
-library(broom)
 library(progress)
 
 ##### ---- Historic Data ---- #####
@@ -9,6 +8,7 @@ library(progress)
 # Function to extract the match links - from season 2017/18 to 'end_year'
 # Saved Data Path: 'rda/historic_matches_links.rda'
 extract_historic_match_links <- function(end_year = NULL){
+  # Non domestic competitions
   ucl_link <- "https://fbref.com/en/comps/8/history/Champions-League-Seasons"
   uel_link <- "https://fbref.com/en/comps/19/history/Europa-League-Seasons"
   
@@ -40,33 +40,41 @@ extract_historic_match_links <- function(end_year = NULL){
 }
 
 
-# Function to extract the historic match reports
-# Saved Data Path: 'rda/historic_match_reports.rda'
-extract_historic_match_reports <- function(){
+# Function to extract the historic match results
+# Saved Data Path: 'rda/historic_match_results.rda'
+extract_historic_match_results <- function(end_year = NULL){
   # Get the start time
   start_time <- Sys.time()
   
-  historic_match_reports <- c()
+  # Non domestic competitions
+  ucl_link <- "https://fbref.com/en/comps/8/history/Champions-League-Seasons"
+  uel_link <- "https://fbref.com/en/comps/19/history/Europa-League-Seasons"
   
-  # Load the historic_matches_links
-  load('rda/historic_matches_links.rda')
-  
-  # Settle progress bar
-  pb <- progress_bar$new(total = length(historic_matches_links))
-  
-  # Extract the match report for every link in historic_matches_links
-  for (match_link in historic_matches_links) {
-    tryCatch({
-      match_report <- fb_match_report(match_url = match_link)
-      historic_match_reports <- bind_rows(historic_match_reports, match_report)
-    }, error = function(e) {
-      NULL # Do nothing if an error occurs
-    })
-    pb$tick() # For progress bar
-  }
+  # Extract the match results
+  historic_match_results <- bind_rows(fb_match_results(country = c("ITA", "ENG", "FRA", "SPA", "GER"),
+                                            gender = "M",
+                                            season_end_year = c(2017:end_year),
+                                            tier = '1st'), 
+                              fb_match_results(country = "ENG",
+                                            gender = "M",
+                                            season_end_year = c(2019:end_year),
+                                            tier = '2nd'),
+                              fb_match_results(country = c("USA", "NED", "POR", "SPA", "GER"),
+                                            gender = "M",
+                                            season_end_year = c(2019:end_year),
+                                            tier = '1st'),
+                              fb_match_results(country = "",
+                                            gender = "M",
+                                            season_end_year = c(2017:end_year),
+                                            non_dom_league_url = ucl_link),
+                              fb_match_results(country = "",
+                                            gender = "M",
+                                            season_end_year = c(2017:end_year),
+                                            non_dom_league_url = uel_link)
+  )
   
   # Save the data into the RDA folder
-  save(historic_match_reports, file = "rda/historic_match_reports.rda")
+  save(historic_match_results, file = "rda/historic_match_results.rda")
   
   # Get the end time
   end_time <- Sys.time()
@@ -168,33 +176,38 @@ extract_szn_match_links <- function(szn = NULL){
 }
 
 
-# Function to extract the current season's match reports
-# Returned Data: curr_szn_match_reports
-extract_szn_match_reports <- function(szn = NULL) {
+# Function to extract the current season's match results
+# Returned Data: curr_szn_matches_results
+extract_szn_match_results <- function(szn){
   # Get the start time
   start_time <- Sys.time()
   
-  # Run the function to get the current season's match links
-  curr_szn_matches_links <- extract_szn_match_links(szn)
+  # Non domestic competitions
+  ucl_link <- "https://fbref.com/en/comps/8/history/Champions-League-Seasons"
+  uel_link <- "https://fbref.com/en/comps/19/history/Europa-League-Seasons"
   
-  curr_szn_match_reports <- c()
-  
-  # Set progress bar
-  pb <- progress_bar$new(total = length(curr_szn_matches_links))
-  
-  # Extract the match report for every link in curr_szn_matches_links
-  for (match_link in curr_szn_matches_links) {
-    
-    # Use tryCatch to catch errors when extracting the match report
-    tryCatch({
-      match_report <- fb_match_report(match_url = match_link)
-      curr_szn_match_reports <- bind_rows(curr_szn_match_reports, match_report)
-    }, error = function(e) {
-      NULL # Do nothing if an error occurs
-    })
-    
-    pb$tick() # For progress bar
-  }
+  # Extract the match results
+  curr_szn_matches_results <- bind_rows(fb_match_results(country = c("ITA", "ENG", "FRA", "SPA", "GER"),
+                                                       gender = "M",
+                                                       season_end_year = szn,
+                                                       tier = '1st'), 
+                                      fb_match_results(country = "ENG",
+                                                       gender = "M",
+                                                       season_end_year = szn,
+                                                       tier = '2nd'),
+                                      fb_match_results(country = c("USA", "NED", "POR", "SPA", "GER"),
+                                                       gender = "M",
+                                                       season_end_year = szn,
+                                                       tier = '1st'),
+                                      fb_match_results(country = "",
+                                                       gender = "M",
+                                                       season_end_year = szn,
+                                                       non_dom_league_url = ucl_link),
+                                      fb_match_results(country = "",
+                                                       gender = "M",
+                                                       season_end_year = szn,
+                                                       non_dom_league_url = uel_link)
+  )
   
   # Get the end time
   end_time <- Sys.time()
@@ -206,9 +219,9 @@ extract_szn_match_reports <- function(szn = NULL) {
   print(paste("Start time: ", start_time))
   print(paste("End time: ", end_time))
   print(paste("Running time: ", round(running_time, 2), "secs", "(", round(running_time/60, 2), "mins)"))
-
-  # Return the match reports
-  return(curr_szn_match_reports)
+  
+  # Return the match links
+  return(curr_szn_matches_results)
 }
 
 
@@ -261,8 +274,3 @@ extract_historic_stat_match_logs <- function(stat = NULL, team_player = NULL){
 
 
 
-#####
-
-
-
-curr_szn_match_reports <- extract_szn_match_reports(2023)
